5a96a82632f6f02af6845e502c30b15bc6ea9d27
Sierra Griffin <sierra.griffin14@gmail.com>
2022-02-16 18:45:33 -0700

    initial

---
+++ OnlineBookStore/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookStore.Models;
+using OnlineBookStore.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnlineBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IBookStoreRepository repo;
+
+        public HomeController(IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        //private BookstoreContext context { get; set; }
+
+        //public HomeController(BookstoreContext temp) => context = temp;
+
+        // GET: /<controller>/
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+
+            var BookListDisplay = new BooksViewModel
+            {
+                Books = repo.Books
+                //.OrderBy(book => book.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+
+                }
+            };
+
+            return View(BookListDisplay);
+
+            //var bookList = repo.Books
+            //    .OrderBy(book => book.Title)
+            //    .Skip((pageNum - 1) * pageSize)
+            //    .Take(pageSize);
+
+            //return View(bookList);
+        }
+
+    }
+}
+
+//lambda is a shortcut to say the result of this function is this...
+//setting up a repository insulates our app from changes in the data store and
+//and can facilitate automated unit testing
---
+++ OnlineBookStore/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OnlineBookStore.Models;
+
+namespace OnlineBookStore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup (IConfiguration temp)
+        {
+            Configuration = temp;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //service that tells ASP.NET to use the MVC setup
+            //necessary for the MVC pattern to be recognized
+            services.AddControllersWithViews();
+
+            services.AddDbContext<BookstoreContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:BookDBConnection"]);
+            });
+
+            services.AddScoped<IBookStoreRepository, EFBookStoreRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //tells ASP.NET to use the files in the wwwroot folder
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //controller first, then action, then id
+                //endpoints.MapControllerRoute(
+                //    name : "default",
+                //    pattern: "{controller=Home}/{action=Index}/{id?}")
+                
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
